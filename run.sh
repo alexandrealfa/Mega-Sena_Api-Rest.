2ec98c6394ad1466d94e56e5d2686b9cd0cb6996
Alexandre Alfa <Profissional@alexandrealfa.com>
2021-04-16 02:56:30 -0400

    adjust run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-python3 -m venv venv
-source venv/bin/activate
-pip install --require-hashes -r requirements.txt
-echo "Digite o seu usuário do postgres =) : "
-read user_name 
-echo "Digite sua senha do usuário do postgres =) :"
-read password
-echo "Digite um nome para o seu novo banco de dados :) :"
-read db_name
-createdb $db_name
-echo -e "FLASK_APP=True\n">.env
-echo -e "FLASK_ENV=development\n">>.env
-echo -e "FLASK_RUN_PORT=5000\n">>.env
-echo -e "FLASK_DB_URI=postgresql://$user_name:$password@localhost:5432/$db_name\n">>.env
-echo "SECRET_KEY=$(openssl rand -base64 32)">>.env
-source .env
-flask db upgrade
+virtualenv venv &&
+source venv/bin/activate &&
+pip install --require-hashes -r requirements.txt &&
+echo "Digite o seu usuário do postgres =) : " &&
+read user_name  &&
+echo "Digite sua senha do usuário do postgres =) :" &&
+read password &&
+echo "Digite um nome para o seu novo banco de dados :) :" &&
+read db_name &&
+echo -e "FLASK_APP=app\n">.env &&
+echo -e "FLASK_ENV=development\n">>.env &&
+echo -e "FLASK_RUN_PORT=5010\n">>.env &&
+echo -e "FLASK_DB_URI=postgresql://$user_name:$password@localhost:5432/$db_name\n">>.env &&
+echo "SECRET_KEY=$(openssl rand -base64 32)">>.env &&
+source .env &&
+flask db upgrade &&
 flask run
