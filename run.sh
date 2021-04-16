c4483f21d91dbd127a9c45fa148ab9ea33a6dac9
Alexandre Alfa <Profissional@alexandrealfa.com>
2021-04-16 03:07:45 -0400

    adjust readm.md

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ echo "Digite um nome para o seu novo banco de dados :) :" &&
 read db_name &&
 echo -e "FLASK_APP=app\n">.env &&
 echo -e "FLASK_ENV=development\n">>.env &&
-echo -e "FLASK_RUN_PORT=5010\n">>.env &&
+echo -e "FLASK_RUN_PORT=5000\n">>.env &&
 echo -e "FLASK_DB_URI=postgresql://$user_name:$password@localhost:5432/$db_name\n">>.env &&
 echo "SECRET_KEY=$(openssl rand -base64 32)">>.env &&
 source .env &&
